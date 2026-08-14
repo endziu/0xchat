997fb744b5e1739f43abfbcc9f82424a747e66c4
endziu <koper.andrzej@gmail.com>
2026-08-14 20:36:21 +0200

    update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(() => ({
-  plugins: [
-    preact(),
-    tailwindcss(),
-  ],
+  plugins: [tailwindcss()],
   resolve: {
     alias: {
       '@': '/src/client',
